bc518b0043d872f240b598b73a382c29637f49df
shoko321 <105973217+shoko321@users.noreply.github.com>
2023-10-07 20:54:19 +0800

    Add files via upload

---
+++ homework10_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "FIT3179_homework9.json"; 
+var vg_2 = "FIT3179_homework10.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
+}).catch(console.error);
